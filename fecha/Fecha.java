5aa039af429334c624066d0023301e53a0d5975e
AylenW <aylen14wyder@gmail.com>
2026-08-08 11:48:12 -0300

    Modificaciones

--- fecha/Fecha.java
+++ fecha/Fecha.java
@@ -1,3 +1,11 @@
+/*Cree una clase llamada Fecha, que incluya tres variables de instancia: un mes (tipo int), un día (tipoint) y un año
+(tipoint). Su clase debe tener un constructor que inicialice las tres variables de instancia, y debe asumir que los
+valores que se proporcionan son correctos. Proporcione un método establecer y un método obtener para cada
+variable de instancia. Proporcione un método mostrarFecha, que muestre el mes, día y año, separados por barras
+diagonales (/). Escriba una aplicación de prueba llamada PruebaFecha, que demuestre las capacidades de la clase
+Fecha estableciendo inicialmente la fecha en “24/02/2012”, luego invocar el método mostrarFecha y
+posteriormente utilizando los métodos de acceso establecer la fecha actual. Sobre escribir el método toString().*/
+
 public class Fecha {
     private int mes;
     private int dia;
